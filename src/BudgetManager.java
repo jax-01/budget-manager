c711c8585efeb28d9990b86f9006ce10f6d7673c
jax-01 <jrcastillo6@up.edu.ph>
2024-08-31 17:49:16 +0800

    Get list of items from user

--- src/BudgetManager.java
+++ src/BudgetManager.java
@@ -1,4 +1,14 @@
 package src;
 
+import java.util.Scanner;
+
 public class BudgetManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder list =  new StringBuilder();
+
+        while (sc.hasNextLine()) {
+            list.append(sc.nextLine()).append("\n");
+        }
+    }
 }
