4b24d530101473c10e57b34ce118c42e510c9792
jax-01 <jrcastillo6@up.edu.ph>
2024-09-05 22:56:24 +0800

    Remove unused code

--- src/BudgetManager.java
+++ src/BudgetManager.java
@@ -31,23 +31,6 @@ public class BudgetManager {
                     System.out.println("Invalid action");
             }
         } while (true);
-
-//        Scanner sc = new Scanner(System.in);
-//        StringBuilder list =  new StringBuilder();
-//        float total = 0f;
-//
-//        while (sc.hasNextLine()) {
-//            list.append(sc.nextLine()).append("\n");
-//        }
-//
-//        String[] listArray = list.toString().split("\n");
-//
-//        for (int i = 0; i < listArray.length; i++) {
-//            total = total + Float.parseFloat(listArray[i].substring(listArray[i].lastIndexOf("$") + 1));
-//        }
-//
-//        System.out.println(list);
-//        System.out.printf("Total: $%.2f", total);
     }
 
     private static void displayMenu() {
