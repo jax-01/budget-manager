aa33d447d969dda5adf0ba768aa5557203c6b3bb
jax-01 <jrcastillo6@up.edu.ph>
2024-09-05 15:09:42 +0800

    Comment out previous code

--- src/BudgetManager.java
+++ src/BudgetManager.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class BudgetManager {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder list =  new StringBuilder();
-        float total = 0f;
-
-        while (sc.hasNextLine()) {
-            list.append(sc.nextLine()).append("\n");
-        }
-
-        String[] listArray = list.toString().split("\n");
-
-        for (int i = 0; i < listArray.length; i++) {
-            total = total + Float.parseFloat(listArray[i].substring(listArray[i].lastIndexOf("$") + 1));
-        }
-
-        System.out.println(list);
-        System.out.printf("Total: $%.2f", total);
+//        Scanner sc = new Scanner(System.in);
+//        StringBuilder list =  new StringBuilder();
+//        float total = 0f;
+//
+//        while (sc.hasNextLine()) {
+//            list.append(sc.nextLine()).append("\n");
+//        }
+//
+//        String[] listArray = list.toString().split("\n");
+//
+//        for (int i = 0; i < listArray.length; i++) {
+//            total = total + Float.parseFloat(listArray[i].substring(listArray[i].lastIndexOf("$") + 1));
+//        }
+//
+//        System.out.println(list);
+//        System.out.printf("Total: $%.2f", total);
     }
 }
